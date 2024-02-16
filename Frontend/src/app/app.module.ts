b78b1ef3099f86c8f349067b180088b9c259a97e
LycanrocHub <juanluis.casado@escuelaestech.es>
2024-02-16 18:56:48 +0100

    Componentes y servicios base

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarUsuarioComponent } from './components/listar-usuario/listar-usuario.component';
+import { GuardarUsuarioComponent } from './components/guardar-usuario/guardar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarUsuarioComponent,
+    GuardarUsuarioComponent
   ],
   imports: [
     BrowserModule,
